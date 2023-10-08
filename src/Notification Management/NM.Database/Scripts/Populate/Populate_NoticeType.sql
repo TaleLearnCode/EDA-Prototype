db064ec44b90fc4c644209df96273a3513cf4fef
Chad Green <chad.green@talelearncode.com>
2023-10-08 01:56:55 -0400

    Adding the initial Notice database

---
+++ src/Notification Management/NM.Database/Scripts/Populate/Populate_NoticeType.sql
@@ -0,0 +1,11 @@
+﻿MERGE INTO Notice.NoticeType AS TARGET
+USING (VALUES (1, 'Order Confirmation'),
+              (2, 'Order Shipped'),
+              (3, 'Order Received'))
+AS SOURCE (NoticeTypeId, NoticeTypeName)
+ON TARGET.NoticeTypeId = SOURCE.NoticeTypeId
+WHEN MATCHED THEN UPDATE SET TARGET.NoticeTypeName = SOURCE.NoticeTypeName
+WHEN NOT MATCHED THEN INSERT (NoticeTypeId,
+                              NoticeTypeName)
+                      VALUES (SOURCE.NoticeTypeId,
+                              SOURCE.NoticeTypeName);
